b8d5844522230f6adf36a5fc526cc7392d5c164d
Alan Lorenzetti <alan.lorenzetti@gmail.com>
2023-07-08 11:05:22 -0400

    added a please wait and done message for processing and results stages, respectively

--- js/deidentifier.js
+++ js/deidentifier.js
@@ -1,39 +1,45 @@
 function deidentifyFiles() {
-    let filesInput = document.getElementById('files');
-    let files = Array.from(filesInput.files); // Convert FileList to Array.
+    return new Promise((resolve, reject) => {
+        let filesInput = document.getElementById('files');
+        let files = Array.from(filesInput.files); // Convert FileList to Array.
 
-    files = shuffle(files); // Shuffle the files.
+        files = shuffle(files); // Shuffle the files.
 
-    let log = "original_name\tdeidentified_name\n"; // Initialize the log with headers.
-    let deidentifiedFiles = [];
+        let log = "original_name\tdeidentified_name\n"; // Initialize the log with headers.
+        let deidentifiedFiles = [];
 
-    for (let i = 0; i < files.length; i++) {
-        let file = files[i];
+        for (let i = 0; i < files.length; i++) {
+            let file = files[i];
 
-        // Get the file extension.
-        let extension = file.name.split('.').pop();
+            // Get the file extension.
+            let extension = file.name.split('.').pop();
 
-        // Create a new file with the deidentified name and the same content.
-        let deidentifiedFile = new File([file], "file" + (i + 1) + "." + extension, {type: file.type});
-        deidentifiedFiles.push(deidentifiedFile);
+            // Create a new file with the deidentified name and the same content.
+            let deidentifiedFile = new File([file], "file" + (i + 1) + "." + extension, {type: file.type});
+            deidentifiedFiles.push(deidentifiedFile);
 
-        // Add a log entry for this file.
-        log += file.name + "\t" + deidentifiedFile.name + "\n";
-    }
+            // Add a log entry for this file.
+            log += file.name + "\t" + deidentifiedFile.name + "\n";
+        }
 
-    // Generate a downloadable link for the deidentified files and the log.
-    let zip = new JSZip();
-    deidentifiedFiles.forEach((file, index) => {
-        zip.file(file.name, file);
-    });
-    zip.file("dictionary.tsv", log);
-    zip.generateAsync({type: "blob"})
-    .then(function(content) {
-        let downloadLink = document.getElementById('downloadLink');
-        let url = window.URL.createObjectURL(content);
-        downloadLink.href = url;
-        downloadLink.download = "deidentifiedFiles.zip";
-        downloadLink.style.display = 'block'; // Make the link visible.
+        // Generate a downloadable link for the deidentified files and the log.
+        let zip = new JSZip();
+        deidentifiedFiles.forEach((file, index) => {
+            zip.file(file.name, file);
+        });
+        zip.file("dictionary.tsv", log);
+        zip.generateAsync({type: "blob"})
+        .then(function(content) {
+            let downloadLink = document.getElementById('downloadLink');
+            let url = window.URL.createObjectURL(content);
+            downloadLink.href = url;
+            downloadLink.download = "deidentifiedFiles.zip";
+            downloadLink.style.display = 'block'; // Make the link visible.
+            resolve(); // resolve the promise
+        })
+        .catch(function(error){
+            reject(error); // reject the promise if there's an error
+        });
     });
 }
 
@@ -85,4 +91,20 @@ function toggleFileList() {
     } else {
         fileListDiv.style.display = 'none';
     }
+}
+
+// Handling the button state to show
+// the .please wait. message
+function startDeidentification() {
+    let deidentifyButton = document.getElementById("deidentifyButton");
+    deidentifyButton.innerHTML = '<i class="fa fa-spinner fa-spin"></i> Processing... Please, wait.';
+    deidentifyButton.disabled = true;
+
+    deidentifyFiles().then(() => {
+        deidentifyButton.innerHTML = '<i class="fa fa-check"></i> Done!';
+    }).catch((error) => {
+        console.error("Deidentification failed:", error);
+        deidentifyButton.innerHTML = '<i class="fa fa-random"></i> Deidentify';
+        deidentifyButton.disabled = false;
+    });
 }
